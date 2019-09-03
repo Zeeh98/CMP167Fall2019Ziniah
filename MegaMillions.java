35bc1d2c3e35da207c2d954ae23c4a244f145b71
Zeeh98 <54686569+Zeeh98@users.noreply.github.com>
2019-09-03 16:46:28 -0400

    Add files via upload

---
+++ MegaMillions.java
@@ -0,0 +1,32 @@
+package Week2;
+
+/**
+ * 
+ * Author: Ziniah Haynes
+ * Description:
+ * Date: September 3,2019
+ * 
+ */
+
+//- import the Scanner.
+import java.util.Random;
+public class MegaMillions {
+	
+	public static void main(String [] args) {
+		Random randGen = new Random();
+		
+		//Declare variable to hold the amount of dollars.
+		//Declare variable for the cost of ticket.
+		//Prompt the user for the amount of dollars to bet on.
+		//Store the amount into amount of dollar variables.
+		//Prompt the user to enter six numbers 
+		//Create a variable for each value.
+		//Randomly generate six numbers and compare them.
+		
+		
+		
+	}
+	
+	
+
+}
