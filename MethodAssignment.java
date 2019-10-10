979b18b850a7424f6f8e9524260cb7aa531eaee8
Zeeh98 <54686569+Zeeh98@users.noreply.github.com>
2019-10-10 16:43:15 -0400

    Add files via upload

---
+++ MethodAssignment.java
@@ -0,0 +1,15 @@
+package Week6;
+
+public class MethodAssignment{
+	
+	public static int prodThree(int num1, int num2, int num3) {
+		
+		return num1 * num2 * num3;
+		
+	}
+	
+	public static void main (String[] args) {
+		System.out.print(prodThree(2, 5, 7));
+	}
+
+}
