d6adb4d110918b720d0ea582ec1517d9381925a6
Zeeh98 <54686569+Zeeh98@users.noreply.github.com>
2021-09-08 01:45:10 -0400

    Update SubtractFive.java

--- SubtractFive.java
+++ SubtractFive.java
@@ -10,23 +10,23 @@ public class SubtractFive {
 		
 		Scanner scnr = new Scanner(System.in);
 		
-		//Introduce the program
+	
 		System.out.println("Welcome this program generates the difference between five numbers.");
 		
-		//Ask the user for their outputs
+		
 		System.out.println("Enter 5 numbers");
 		
-		//Wait for the users output.
+		
 		int num1 = scnr.nextInt();
 		int num2 = scnr.nextInt();
 		int num3 = scnr.nextInt();
 		int num4 = scnr.nextInt();
 		int num5 = scnr.nextInt();
 		
-		//Calculate
+		
 		int result = num5 - num4 - num3 - num2 -num1;
 		
-		//Display result
+		
 		System.out.println("Result is " +  result);
 		
 		
